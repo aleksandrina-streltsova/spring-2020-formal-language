ce3c3955d93027cad093013d4386945ce79c16d7
aleksandrina-streltsova <aleksandrina.strelczova@mail.ru>
2020-05-20 13:11:41 +0300

    Finish parser implementation

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from antlr4 import *
 from argparse import ArgumentParser, FileType
-from src.language.antlr.query import print_script_tree, script_is_correct
+from src.language.antlr.query import print_script_tree, script_is_correct, run_script
 
 
 def main():
     console_parser = ArgumentParser()
+    console_parser.add_argument('-r', '--run', action='store_true', help='run script')
     console_parser.add_argument('-i', nargs='?', type=FileType('r'), dest='infile', help='parse script from file')
     console_parser.add_argument('-o', nargs='?', type=FileType('w'), dest='outfile', help='write tree to file')
     args = console_parser.parse_args()
@@ -12,13 +13,20 @@ def main():
         stream = InputStream(args.infile.read())
     else:
         stream = StdinStream()
-    if args.outfile:
-        print_script_tree(stream, args.outfile)
-    else:
-        if script_is_correct(stream):
-            print("script is correct")
+    if args.run:
+        result = run_script(stream)
+        if args.outfile:
+            args.outfile.write(result)
         else:
-            print("script is incorrect")
+            print(result)
+    else:
+        if args.outfile:
+            print_script_tree(stream, args.outfile)
+        else:
+            if script_is_correct(stream):
+                print("script is correct")
+            else:
+                print("script is incorrect")
 
 
 if __name__ == '__main__':
